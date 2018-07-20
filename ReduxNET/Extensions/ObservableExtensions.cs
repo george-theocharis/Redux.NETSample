73193345bdede1a1867a52d81c0e249688e2f492
giorgosTh <gt@anaxoft.com>
2018-07-20 15:27:39 +0300

    Observable Extension to hold and dispose subscriptions Fab button clears or loads posts for testing

---
+++ ReduxNET/Extensions/ObservableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Disposables;
+
+namespace ReduxNET.Extensions
+{
+    public static class ObservableExtensions
+    {
+        public static TDisposable DisposeWith<TDisposable>(this TDisposable observable, CompositeDisposable disposables) where TDisposable : class, IDisposable
+        {
+            if (observable != null)
+                disposables.Add(observable);
+
+            return observable;
+        }
+    }
+}
--- ReduxNET/MainActivity.cs
+++ ReduxNET/MainActivity.cs
@@ -12,25 +12,33 @@ using ReduxNET.Actions;
 using Android.Support.Transitions;
 using Android.Support.Constraints;
 using ReduxNET.PostDetails;
+using Android.Support.Design.Widget;
+using System.Reactive.Disposables;
+using ReduxNET.Extensions;
 
 namespace ReduxNET
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private FloatingActionButton _fab;
         private ConstraintLayout _container;
         private ProgressBar _loading;
         private RecyclerView _posts;
         private PostsAdapter _adapter;
 
+        private CompositeDisposable _disposables = new CompositeDisposable();
+
         protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
+        {
+            base.OnCreate(savedInstanceState);
 
-			SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.activity_main);
 
-			Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
+
+            _fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
 
             _container = FindViewById<ConstraintLayout>(Resource.Id.container);
             _loading = FindViewById<ProgressBar>(Resource.Id.loading);
@@ -39,17 +47,54 @@ namespace ReduxNET
             _posts.SetLayoutManager(new LinearLayoutManager(ApplicationContext, LinearLayoutManager.Vertical, false));
             _adapter = new PostsAdapter();
             _posts.SetAdapter(_adapter);
-
-            App.App.Store.DistinctUntilChanged(state => state.PostsState.Posts).Subscribe(state => Render(state.PostsState));
-            App.App.Store.DistinctUntilChanged(state => state.PostsState.SelectedPostId).Skip(1).Subscribe(state => Render(state.PostsState.SelectedPostId));
         }
 
         protected override void OnStart()
         {
             base.OnStart();
+            SetupSubscriptions();
+            SetupEventHandlers();
+            Start();
+        }
 
+        private void Start()
+        {
             if (_adapter.ItemCount == 0)
                 App.App.Store.Dispatch(AsyncActionCreators.FetchPosts());
+        }
+
+        private void SetupEventHandlers()
+        {
+            Observable.FromEventPattern(_fab, "Click")
+                      .Subscribe(e =>
+                      {
+                          if (_adapter.ItemCount > 0)
+                              App.App.Store.Dispatch(new PostsFetched() { Posts = new System.Collections.Generic.List<Post>() });
+                          else
+                              App.App.Store.Dispatch(AsyncActionCreators.FetchPosts());
+                      })
+                      .DisposeWith(_disposables);
+        }
+
+        private void SetupSubscriptions()
+        {
+            App.App.Store
+                .DistinctUntilChanged(state => state.PostsState.Posts)
+                .Subscribe(state => Render(state.PostsState))
+                .DisposeWith(_disposables);
+
+
+            App.App.Store
+                .DistinctUntilChanged(state => state.PostsState.SelectedPostId)
+                .Skip(1)
+                .Subscribe(state => Render(state.PostsState.SelectedPostId))
+                .DisposeWith(_disposables);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            _disposables.Clear();
         }
 
         private void Render(PostsState state)
